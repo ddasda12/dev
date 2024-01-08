444855cf03fa74525b9bf2080cd818196ee93f24
ddasda12 <156016621+ddasda12@users.noreply.github.com>
2024-01-08 15:48:34 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from dhooks import Webhook
-import schedule
-import time
 
 hook = Webhook(
     'https://discord.com/api/webhooks/1194012870078378054/eevDHgzT9OGSehwGUDyYzXhZZtZhkYB4_w_rx4lg_AVFyMbAQOudZEgtxAdQhVGXKgfx'
@@ -14,12 +12,13 @@ def hello():
     hook.send('hello')
     return 'Hello, world'
 
-
+@app.route('/test')
+def test():
+    hook.send('hello')  # Moved this line before the return statement
+    return 'Test'
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
-    
     run()
-
