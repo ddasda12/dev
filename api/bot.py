cec4f401a8a605d372fe40b00298153d1d115ed1
ddasda12 <156016621+ddasda12@users.noreply.github.com>
2024-01-08 15:21:42 -0500

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,1 +1,8 @@
+from dhooks import Webhook
+import time
+hook=Webhook('https://discord.com/api/webhooks/1194012870078378054/eevDHgzT9OGSehwGUDyYzXhZZtZhkYB4_w_rx4lg_AVFyMbAQOudZEgtxAdQhVGXKgfx')
 
+
+while True:
+    hook.send('hello')
+    time.sleep(10)
