b93afa9bf41ba173ec0c62a5d7480441dcdea45b
ddasda12 <156016621+ddasda12@users.noreply.github.com>
2024-01-08 15:34:37 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,23 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask
 from dhooks import Webhook
 
-hook=Webhook('https://discord.com/api/webhooks/1194012870078378054/eevDHgzT9OGSehwGUDyYzXhZZtZhkYB4_w_rx4lg_AVFyMbAQOudZEgtxAdQhVGXKgfx')
+hook = Webhook(
+    'https://discord.com/api/webhooks/1194012870078378054/eevDHgzT9OGSehwGUDyYzXhZZtZhkYB4_w_rx4lg_AVFyMbAQOudZEgtxAdQhVGXKgfx'
+)
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    hook.send('hello')
-    return 'Hello, world'
+  hook.send('hello')
+  return 'Hello, world'
 
 
 @app.route('/test')
 def test():
-    return 'Test'
-    hook.send('hello')
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+  hook.send('hello')  # Moved this line before the return statement
+  return 'Test'
 
 
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+
+if __name__ == '__main__':
+  run()
