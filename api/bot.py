b36cabd7b18a725672fe76dbccaf819a7b117d7e
ddasda12 <156016621+ddasda12@users.noreply.github.com>
2024-01-08 15:22:10 -0500

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,5 +1,6 @@
 from dhooks import Webhook
 import time
+from index import keep_alive
 hook=Webhook('https://discord.com/api/webhooks/1194012870078378054/eevDHgzT9OGSehwGUDyYzXhZZtZhkYB4_w_rx4lg_AVFyMbAQOudZEgtxAdQhVGXKgfx')
 
 
